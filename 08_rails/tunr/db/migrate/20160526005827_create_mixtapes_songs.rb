0f93c0c96b580328dc4a11cff9dfee483f4bab47
ga-wolf <jack.jeffress@ga.co>
2016-05-27 14:51:15 +1000

    Finished login app with PostgreSQL

--- 08_rails/tunr/db/migrate/20160526005827_create_mixtapes_songs.rb
+++ 08_rails/tunr/db/migrate/20160526005827_create_mixtapes_songs.rb
@@ -1,6 +1,9 @@
+# rails generate migration create_mixtapes_songs mixtape_id:integer song_id:integer
 class CreateMixtapesSongs < ActiveRecord::Migration
   def change
+    # :id => false - this means no primary key!
     create_table :mixtapes_songs, :id => false do |t|
+      # This table will only have foreign keys
       t.integer :mixtape_id
       t.integer :song_id
     end
