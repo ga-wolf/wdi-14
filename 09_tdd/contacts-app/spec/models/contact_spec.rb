c6303664262c3383390337693bb529d109682e11
ga-wolf <jack.jeffress@ga.co>
2016-06-29 16:07:28 +1000

    Added tests

---
+++ 09_tdd/contacts-app/spec/models/contact_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+RSpec.describe Contact, type: :model do
+  it "has a valid factory" do
+    contact = build :contact
+    expect(contact).to be_valid
+  end
+  it "is valid with a first_name, last_name and an email" do
+    contact = build :contact
+    expect(contact).to be_valid
+  end
+  it "is invalid without a first_name" do
+    contact = build :contact, first_name: nil
+    contact.valid? # Make the model generate error messages
+    expect(contact.errors[:first_name]).to include("can't be blank")
+  end
+  it "is invalid without a last_name" do
+    contact = build :contact, last_name: nil
+    contact.valid?
+    expect(contact.errors[:last_name]).to include("can't be blank")
+  end
+  it "is invalid without an email" do
+    contact = build :contact, email: nil
+    contact.valid?
+    expect(contact.errors[:email]).to include("can't be blank")
+  end
+  it "is invalid with a duplicate email address" do
+    create :contact, email: "marx@ga.co"
+    contact = build :contact, email: "marx@ga.co"
+    contact.valid?
+    expect(contact.errors[:email]).to include("has already been taken")
+  end
+  it "returns a contact's full name as a string" do
+    contact = build :contact, first_name: "Gummo", last_name: "Marx"
+    expect(contact.name).to eq("Gummo Marx")
+  end
+  context "filter last name by letter" do
+    before :each do
+      @smith = Contact.create(first_name: "John", last_name: "Smith", email: "jsmith@ga.co")
+      @jones = Contact.create(first_name: "Tim", last_name: "Jones", email: "tjones@ga.co")
+      @johns = Contact.create(first_name: "Tim", last_name: "Johns", email: "tjohns@ga.co")
+    end
+    it "returns a sorted array of contacts that match" do
+      expect(Contact.by_letter("J")).to eq([@johns, @jones])
+    end
+    it "omits contacts who don't match" do
+      expect(Contact.by_letter("J")).not_to include(@smith)
+    end
+  end
+end
